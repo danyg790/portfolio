5f2bd42d64c459fe902839800e7a6e22e00a5d14
danyg790 <dany-g@live.ca>
2025-10-24 21:39:38 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PortfolioNavbar from './components/Navbar';
 import Home from './pages/Home';
-//import About from './pages/About';
-//import Projects from './pages/Projects';
-//import Contact from './pages/Contact';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
