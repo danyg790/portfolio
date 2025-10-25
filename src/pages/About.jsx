535aaf3f0aedbe1e5ffb946704feb4179dd5b09b
danyg790 <dany-g@live.ca>
2025-10-24 21:30:44 -0400

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Section from '../components/Section';
+
+const About = () => {
+  return (
+    <Section id="about" title="About Me">
+      <p>
+        Hi! I'm Daniel Giesbrecht, a passionate front-end developer focused on building elegant and responsive interfaces.
+      </p>
+    </Section>
+  );
+};
+
+export default About;
